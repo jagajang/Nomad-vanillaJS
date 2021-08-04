bf9e62582aa0bd89fabd24aa9bee08e4c4155d6c
Sungbin Ko <kosb7544@gmail.com>
2021-08-04 22:39:00 +0900

    added weather & city

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,23 @@
+const apiKey = "e6a91af91f9fb32ddacff8545dffd642"
 function onGeoSuccess(position) {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+
+    const weather = document.querySelector("#weather")
+    const cityInfo = weather.querySelector("#city-info")
+    const weatInfo = weather.querySelector("#weat-info")
+
+    fetch(url).then(response => response.json()).then(data => {
+        weather.classList.remove("hidden")
+
+        cityInfo.innerText = data.name
+        weatInfo.innerText = data.weather[0].main
+    })
+
+
+    cityInfo.innerText = data
 }
 
 function onGeoFail(position) {
