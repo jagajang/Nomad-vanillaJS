895effe35543460457d87a8e31ba2095e40f5e5d
Sungbin Ko <kosb7544@gmail.com>
2021-08-04 15:08:45 +0900

    initial

---
+++ script.js
@@ -0,0 +1,34 @@
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input")
+const loginGreet = document.querySelector("#greeting")
+
+function onLoginFormSubmit(event) {
+    const username = loginInput.value
+    greetString(username)
+
+    event.preventDefault()
+    
+    loginForm.classList.add("hidden")
+    loginGreet.classList.remove("hidden")
+
+    localStorage.setItem("username", username)
+
+    return username
+}
+
+function greetString(username) {
+    loginGreet.innerText = `Hi ${username}!`
+}
+
+console.log(localStorage)
+
+const savedUsername = localStorage.getItem("username")
+
+if(savedUsername === null) {
+    loginForm.classList.remove("hidden")
+    loginForm.addEventListener("submit", onLoginFormSubmit)
+}
+else {
+    loginGreet.classList.remove("hidden")
+    greetString(savedUsername)
+}
